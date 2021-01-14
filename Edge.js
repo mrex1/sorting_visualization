6d572cb85f41bc7a643727f97fa623e89ad8b44d
rex <rexmo@hku.hk>
2021-01-14 18:45:16 +0800

    drawing edges behind vertices

--- Edge.js
+++ Edge.js
@@ -9,10 +9,12 @@ class Edge extends VisualizeObject {
     render(ctx, origin) {
         let { x: sx, y: sy } = this.processCoord(this.start, origin)
         let { x: ex, y: ey } = this.processCoord(this.end, origin)
+        ctx.globalCompositeOperation = 'destination-over'
         ctx.beginPath()
         ctx.lineWidth = this.width
         ctx.moveTo(sx, sy)
         ctx.lineTo(ex, ey)
         ctx.stroke()
+        ctx.globalCompositeOperation = 'source-over'
     }
 }
